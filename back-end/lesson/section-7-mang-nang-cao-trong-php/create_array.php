a3e264a47f4b19e13acac6d8002cfdbd8a414b0f
thisisminh172 <65597101+thisisminh172@users.noreply.github.com>
2020-06-16 15:38:56 +0700

    small changes

--- back-end/lesson/section-7-mang-nang-cao-trong-php/create_array.php
+++ back-end/lesson/section-7-mang-nang-cao-trong-php/create_array.php
@@ -1,21 +1,20 @@
 <?php
 #tạo mảng rỗng
-$error = array();
-$error['username']='Bạn không được để trống tên đăng nhập';
+$error             = array();
+$error['username'] = 'Bạn không được để trống tên đăng nhập';
 #tạo mảng với key mặc định
-$list_odd = array(1,3,5);
+$list_odd = array(1, 3, 5);
 #Tạo mảng với key xác định
 //User list: id, họ tên, email
 $list_user = array(
-    'id'=>1,
-    'fullname'=>'Lê Minh',
-    'email'=>'minh@gmail.com'
+    'id'       => 1,
+    'fullname' => 'Lê Minh',
+    'email'    => 'minh@gmail.com'
 );
 
 //===========
 //output
 //===========
 echo "<pre>";
-print_r ($error);
+print_r($error);
 echo "</pre>";
-?>
